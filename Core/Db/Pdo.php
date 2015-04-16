a45fbc8002fd63110ca94ea2364302e738666e4b
OuHaixiong <258333309@163.com>
2015-04-16 18:12:53 +0800

    数据库pdo

--- Core/Db/Pdo.php
+++ Core/Db/Pdo.php
@@ -1,6 +1,23 @@
 <?php
 
-class Db_Pdo
+class Db_Pdo extends PDO
 {
+    public function __construct() ($dsn, $username, $password, array $driver_options = array(), $charset = 'utf8') {
+        $db = new PDO('mysql:host=myhost;dbname=mydb', 'login', 'password', array(PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES \'UTF8\''));
+    }
+    
+    $con = new PDO("mysql:dbname=dbname;host=some.ip", "user", "pass", array(
+        PDO::MYSQL_ATTR_USE_BUFFERED_QUERY => true,
+    ));
+    
+    $dsn = 'mysql:host=localhost;dbname=testdb';
+    $username = 'username';
+    $password = 'password';
+    $options = array(
+        PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8',
+    );
+    
+    $dbh = new PDO($dsn, $username, $password, $options);
+    
     
 }
