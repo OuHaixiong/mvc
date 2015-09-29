4ebcc64dfdee19e20245337f573267bcfef12ee9
OuHaixiong <258333309@163.com>
2015-09-29 11:33:47 +0800

    测试同步

--- public/index.php
+++ public/index.php
@@ -1,7 +1,7 @@
 <?php
 ini_set('session.cookie_path', '/'); // 设置cookie保存在跟目录
 ini_set('session.cookie_domain', '.mvc.com'); // cookie保存在主域下
-ini_set('session.cookie_lifetime', '1800');
+ini_set('session.cookie_lifetime', '1800'); // 设置cookie的生命周期（时间）
 // 上面三句是跨子域，保存cookie
 header('Content-Type:text/html;charset=utf-8'); //定义字符集
 
