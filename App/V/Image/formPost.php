58d5f7987df36f018717d6345ec43d28b6d9fbd9
OuHaixiong <258333309@163.com>
2015-11-19 11:01:45 +0800

    修改模板

--- App/V/Image/formPost.php
+++ App/V/Image/formPost.php
@@ -1,4 +1,4 @@
 <form action="#" method="post" enctype="multipart/form-data">
-<div><input type="file" name="file_data" /></div>
-<div><input type="submit" value="上传图片" /></div>
+ <div><input type="file" name="file_data" /></div>
+ <div><input type="submit" value="上传图片" /></div>
 </form>
