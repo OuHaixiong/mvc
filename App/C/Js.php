c4fd427cd04b6c40bd56958c7f79538f00d6dfb0
Bear <258333309@163.com>
2016-12-01 10:59:55 +0800

    summernote 在线文本编辑器

--- App/C/Js.php
+++ App/C/Js.php
@@ -8,7 +8,21 @@
  */
 class C_Js extends BController
 {
+    /**
+     * 弹框测试
+     */
     public function popup() {
         $this->render();
     }
+    
+    /**
+     * Summernote 在线编辑器测试
+     */
+    public function summernote() {
+        if (Common_Tool::isPost()) {
+            var_dump($_POST);exit;
+        }
+        $this->render();
+    }
+    
 }
